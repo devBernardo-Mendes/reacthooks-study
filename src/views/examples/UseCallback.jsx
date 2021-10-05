e885d3b4202f6a454b9a0703ddc9c714777ebc43
devBernardo-Mendes <devbernardomr@gamil.com>
2021-10-05 17:57:01 -0300

    initial commit

---
+++ src/views/examples/UseCallback.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+
+const UseCallback = (props) => {
+    const [count, setcount] = useState(0)
+    return (
+        <div className="UseCallback">
+            <PageTitle
+                title="Hook UseCallback"
+                subtitle="Retorna uma função memoizada!"
+            />
+            <div className="center">
+                <span className="text">{count}</span>
+
+            </div>
+        </div>
+    )
+}
+
+
+export default UseCallback
---
+++ src/views/examples/UseState.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const UseState = (props) => {
+    const [count, setcount] = useState(0)
+    const [name, setName] = useState("")
+
+    return (
+        <div className="UseState">
+            <PageTitle
+                title="Hook UseState"
+                subtitle="Estado em componentes funcionais!" />
+
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn"
+                        onClick={() => setcount(count - 1)}>-1</button>
+                    <button className="btn"
+                        onClick={() => setcount(count + 1)}>+1</button>
+                    <button className="btn"
+                        onClick={() => setcount(current => current + 1000)}>+1000</button>
+                </div>
+            </div>
+            <SectionTitle title="Exercício #02" />
+            <input type="text" className="input"
+                value={name} onChange={e => setName(e.target.value)}
+            />
+
+        </div>
+    )
+}
+
+export default UseState
+// componentet nao controlado é aquele que nao tem nenhum estado no input
