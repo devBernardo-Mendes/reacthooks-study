32e436d665c67ca090f87dcb00568dd6c25920f6
devBernardo-Mendes <devbernardomr@gmail.com>
2021-10-14 21:46:38 -0300

    feat: :sparkles: Study useCallBack

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,40 +1,29 @@
-import React, { useState, useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import PageTitle from "../../components/layout/PageTitle";
-import UseCallbackButtons from "../examples/UseCallBackButtons";
+import UseCallBackButtons from "./UseCallBackButtons";
 
 const UseCallback = (props) => {
   const [count, setcount] = useState(0);
-    return (
-        <div className="UseCallback">
-            <PageTitle
-                title="Hook UseCallback"
-                subtitle="Retorna uma função memoizada!"
-            />
-            <div className="center">
-                <span className="text">{count}</span>
-            </div>
-        </div>
-    )
-    const inc = useCallback(
-      function (delta) {
-        setcount(curr => curr + delta);
-      },
-      [setcount]
-    );
-  
-    return (
-      <div className="UseCallback">
-        <PageTitle
-          title="Hook UseCallback"
-          subtitle="Retorna uma função memoizada!"
-        />
-        <div className="center">
-          <span className="text">{count}</span>
-          <UseCallbackButtons inc={inc} />
-        </div>
+
+  const inc = useCallback(
+    function (delta) {
+      setcount((curr) => curr + delta);
+    },
+    [setcount]
+  );
+
+  return (
+    <div className="UseCallback">
+      <PageTitle
+        title="Hook UseCallback"
+        subtitle="Retorna uma função memoizada!"
+      />
+      <div className="center">
+        <span className="text">{count}</span>
+        <UseCallBackButtons inc={inc} />
       </div>
-    );
-}
+    </div>
+  );
 };
 
 export default UseCallback;
