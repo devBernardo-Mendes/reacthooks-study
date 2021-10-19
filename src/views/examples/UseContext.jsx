92ba73240d6a035aa6b38ee17aa852a9b2c6b4c1
devBernardo-Mendes <devbernardomr@gmail.com>
2021-10-19 11:30:41 -0300

    feat: :sparkles: Create fuction that add value in number

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useEffect, useContext } from "react";
 
 import PageTitle from "../../components/layout/PageTitle";
 import DataContext from "../../data/DataContext";
+import SectionTitle from "../../components/layout/SectionTitle";
+import { AppContenxt } from "../../data/Store";
 
 const UseContext = (props) => {
   const { state, setState } = useContext(DataContext);
@@ -14,12 +15,22 @@ const UseContext = (props) => {
     });
   }
 
+  const { number, text, setNumber, setText } = useContext(AppContenxt);
+
+  useEffect(function () {
+    if (number > 1250) {
+      setText("Eita");
+    }
+  }, [number, setText]);
+
   return (
     <div className="UseContext">
       <PageTitle
         title="Hook UseContext"
         subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
       />
+
+      <SectionTitle title="Exercicio #01" />
       <div className="center">
         <span className="text">{state.text}</span>
         <span className="text">{state.number}</span>
@@ -31,6 +42,19 @@ const UseContext = (props) => {
           <buton className="btn" onClick={() => addNumber(1)}>
             +1
           </buton>
+        </div>
+      </div>
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        <span className="text">{text}</span>
+        <span className="text">{number}</span>
+        <div>
+          <button className="btn" onClick={() => setNumber(number - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setNumber(number + 1)}>
+            +1
+          </button>
         </div>
       </div>
     </div>
