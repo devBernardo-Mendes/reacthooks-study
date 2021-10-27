2b6f4c0cd9757731e3b3465b4dc0bac404a3946e
devBernardo-Mendes <devbernardomr@gmail.com>
2021-10-27 18:23:11 -0300

    feat: :sparkles: Created config to apply the type and pass a function

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,32 +1,8 @@
 import React, { useReducer } from "react";
 import PageTitle from "../../components/layout/PageTitle";
+import {initialState, reducer} from "../../store/config"
 
-const initialState = {
-  cart: [],
-  products: [],
-  user: null,
-  //foco...
-  number: 0,
-};
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "number_add2":
-      return { ...state, number: state.number + 2 };
-    case "number_mult7":
-      return { ...state, number: state.number * 7 };
-    case "number_share25":
-      return { ...state, number: state.number / 25 };
-    case "login":
-      return { ...state, user: { name: action.payload } };
-    case "int":
-      return { ...state, number: parseInt(state.number) };
-    case "number_AddN":
-      return { ...state, number: state.number + action.payload };
-    default:
-      return state;
-  }
-}
 const UseReducer = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
