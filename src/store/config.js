bc295a7a6e1a3f720e8523adb0c734fd39a72f79
devBernardo-Mendes <devbernardomr@gmail.com>
2021-11-06 19:14:30 -0300

    style: :art: Formatted the code

--- src/store/config.js
+++ src/store/config.js
@@ -1,26 +1,26 @@
 export const initialState = {
-    cart: [],
-    products: [],
-    user: null,
-    //foco...
-    number: 0,
-  };
+  cart: [],
+  products: [],
+  user: null,
+  //foco...
+  number: 0,
+};
 
-  export function reducer(state, action) {
-    switch (action.type) {
-      case "number_add2":
-        return { ...state, number: state.number + 2 };
-      case "number_mult7":
-        return { ...state, number: state.number * 7 };
-      case "number_share25":
-        return { ...state, number: state.number / 25 };
-      case "login":
-        return { ...state, user: { name: action.payload } };
-      case "int":
-        return { ...state, number: parseInt(state.number) };
-      case "number_AddN":
-        return { ...state, number: state.number + action.payload };
-      default:
-        return state;
-    }
+export function reducer(state, action) {
+  switch (action.type) {
+    case "number_add2":
+      return { ...state, number: state.number + 2 };
+    case "number_mult7":
+      return { ...state, number: state.number * 7 };
+    case "number_share25":
+      return { ...state, number: state.number / 25 };
+    case "login":
+      return { ...state, user: { name: action.payload } };
+    case "int":
+      return { ...state, number: parseInt(state.number) };
+    case "number_AddN":
+      return { ...state, number: state.number + action.payload };
+    default:
+      return state;
   }
+}
